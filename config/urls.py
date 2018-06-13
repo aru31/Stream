002ae16aee9dd4e9485ae878f886d0b9f13e3b2a
Arpit Gupta <guptarpit1997@gmail.com>
2018-06-14 02:25:02 +0530

    Made React Login Form

---
+++ config/urls.py
@@ -0,0 +1,50 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
+from django.views import generic
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework import views, serializers, status
+from rest_framework.response import Response
+
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+
+class EchoView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = MessageSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED)
+
+
+urlpatterns = [
+    url(r'^$', generic.RedirectView.as_view(
+         url='/api/', permanent=False)),
+    url(r'^api/$', get_schema_view()),
+    url(r'^api/auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/auth/token/obtain/$', TokenObtainPairView.as_view()),
+    url(r'^api/auth/token/refresh/$', TokenRefreshView.as_view()),
+    url(r'^api/echo/$', EchoView.as_view())
+]
+
---
+++ env/lib/python3.6/rlcompleter.py
@@ -0,0 +1,1 @@
+/home/aruroxx31/anaconda3/lib/python3.6/rlcompleter.py
