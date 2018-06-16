394e1e04a09a093a6e99742b1fc25e4071b4a9d9
Arpit Gupta <guptarpit1997@gmail.com>
2018-06-17 03:05:28 +0530

    Finally Registered

--- stream/views.py
+++ stream/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from rest_framework import permissions
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ class CreateUserView(generics.ListCreateAPIView):
 	model = get_user_model()
 	queryset = model.objects.all()
 	serializer_class = UserSerializer
-
+	permission_classes = (permissions.AllowAny,)
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
 
